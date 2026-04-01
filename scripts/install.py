779dd310de49bf121a66f1d0eedbb42628ae402b
Coooolfan <78391548+Coooolfan@users.noreply.github.com>
2026-04-01 21:55:55 +0800

    Update default tag version to 0.3.0

--- scripts/install.py
+++ scripts/install.py
@@ -25,7 +25,7 @@ from pathlib import Path
 # ---------------------------------------------------------------------------
 
 GITHUB_REPO = "Coooolfan/onlyboxes"
-DEFAULT_TAG = "0.2.1"
+DEFAULT_TAG = "0.3.0"
 COMPOSE_TEMPLATE_URL = (
     "https://raw.githubusercontent.com/{repo}/{tag}/scripts/docker-compose.install.yml"
 )
